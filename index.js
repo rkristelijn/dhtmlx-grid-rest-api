61f7191a5a680f233e5e3e2eb6ae84116cd7ce06
Remi Kristelijn <rkristelijn@gmail.com>
2018-05-16 10:31:30 +0200

    working version but sends the data twice

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/cms');
+let db = mongoose.connection;
+
+let Schema = mongoose.model('contacts', mongoose.Schema({
+  fname:'string',lname:'string',email:'string'
+}));
+
+db.on('error', console.error.bind(console, 'Mongoose:'));
+db.once('open', () => {
+  console.log('Connected to mongoose');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/codebase', express.static(path.join(__dirname, 'codebase')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
@@ -9,6 +26,78 @@ app.use('/icons', express.static(path.join(__dirname, 'codebase/icons')));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.route('/connector/contacts')
+  .get((req, res) => {
+    console.log('get');
+  })
+  .post((req, res) => {
+    console.log('post');
+  });
+
+app.route('/connector/contacts/:id')
+  .get((req, res) => {
+    console.log('get:', req.params.id);
+  })
+  .put((req, res) => {
+    let data = req.body;
+    delete data.gr_id;
+    delete data.head;
+    console.log('put:', req.params.id, data);
+
+
+    let newContact = new Schema(data);
+    console.log(newContact);
+    newContact.save((err, contact) => {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('created:',contact);
+        res.send(contact);
+      }
+    });
+
+    // Schema.find({}, (err, contact) => {
+    //   if (!contact) {
+    //     console.log('put:creating...', req.body);
+    //     let newContact = new Schema(req.body);
+    //     newContact.save((err, contact) => {
+    //       res.send(contact);
+    //     });
+    //   } else {
+    //     res.send(contact);
+    //   }
+    // });
+  })
+  .delete((req, res) => {
+    console.log('delete', req.params.id);
+  });
+// app.get('/connector/contacts', (req, res) => {
+//   let Schema = mongoose.model('contacts', mongoose.Schema({}));
+//   let contacts = Schema.find({});
+//   res.sendStatus(200).end(contacts);
+// });
+// app.put('/connector/:id', (req, res) => {
+//   console.log(req.data);
+//   // let Schema = mongoose.model('contacts', mongoose.Schema({}));
+//   // let id = req.data.id;
+//   // let action = req.data.action;
+//   // let data = req.body.data;
+//   // delete data.head;
+
+//   // Schema.find((dbFindErr, document) => {
+//   //   if(document) {
+//   //     document.save(data);
+//   //     res.sendStatus(200);
+//   //     res.end(document);
+//   //   } else {
+//   //     let newDocument = new Schema(data);
+//   //     newDocument.save();
+//   //     res.sendStatus(200);
+//   //     res.end(newDocument);
+//   //   }
+//   // });
+// });
 
 app.listen(3000, () => {
   console.log('listening on *:3000');
--- public/dhx.js
+++ public/dhx.js
@@ -2,7 +2,7 @@ dhtmlxEvent(window, "load", function () {
   var layout = new dhtmlXLayoutObject(document.body, "2U");
   layout.cells("a").setText("Contacts");
   layout.cells("b").setText("Contact Details");
-  layout.cells("b").setWidth(500);
+  layout.cells("b").setWidth(400);
 
   var menu = layout.attachMenu();
   menu.setIconsPath("icons/");
@@ -13,8 +13,66 @@ dhtmlxEvent(window, "load", function () {
   toolbar.loadStruct("public/data/toolbar.xml");
 
   var contactsGrid = layout.cells("a").attachGrid();
-  contactsGrid.attachHeader("#text_filter,#text_filter,#text_filter");
+  //contactsGrid.attachHeader("#text_filter,#text_filter,#text_filter");
 
+  //contactsGrid.enableEditEvents(true,false,true);
   contactsGrid.init();
+  //contactsGrid.post("data.php?id=1","data=10",null,"json");
   contactsGrid.load("public/data/grid.json", "json");
+  // contactsGrid.setEditable(true);
+
+  contactForm = layout.cells("b").attachForm();
+  contactForm.loadStruct("public/data/form.xml");
+  contactForm.bind(contactsGrid);
+
+  var dpg = new dataProcessor("/connector/contacts/");         //inits dataProcessor
+  dpg.enableDebug(true);
+  dpg.init(contactsGrid);   //associates the dataProcessor instance with the grid
+  dpg.setTransactionMode("REST");
+
+  dpg.attachEvent("onAfterUpdate", function (sid, action, tid, tag) {
+    if (action == "inserted") {
+      //console.log('inserted');
+      //contactsGrid.selectRowById(tid);        //selects the newly-created row
+      //contactForm.setFocusOnFirstActive();//set focus to the 1st form's input
+    } else {
+      //console.log('other action:', sid, action, tid, tag);
+      //contactForm.setFormData(tag);
+      return false;
+    }
+  });
+
+
+  contactForm.attachEvent("onButtonClick", function (name) {
+    contactForm.save();     //sends the values of the updated row to the server
+    console.log('saving...');
+    return false;
+  });
+  // contactForm.attachEvent("onblur", function () {
+  //   contactForm.save();
+  // })
+
+  toolbar.attachEvent("onclick", function (id) {
+    if (id == "newContact") {
+      console.log('newContact');
+      var rowId = contactsGrid.uid();
+      var pos = contactsGrid.getRowsNum();
+      contactsGrid.addRow(rowId, ["New contact", "", ""], pos);
+      contactsGrid.selectRowById(rowId);
+      contactForm.setFocusOnFirstActive();
+    };
+    if (id == "delContact") {
+      console.log('delContact');
+      var rowId = contactsGrid.getSelectedRowId();
+      var rowIndex = contactsGrid.getRowIndex(rowId);
+      if (rowId != null) {
+        contactsGrid.deleteRow(rowId);
+        if (rowIndex != (contactsGrid.getRowsNum() - 1)) {
+          contactsGrid.selectRow(rowIndex + 1, true);
+        } else {
+          contactsGrid.selectRow(rowIndex - 1, true)
+        }
+      }
+    }
+  });
 });
